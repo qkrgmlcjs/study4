d7e4c7495e7e0b53eb04a9a21be39dac6daa196b
qkrgmlcjs <32252143+qkrgmlcjs@users.noreply.github.com>
2017-10-18 09:19:03 +0900

    remote  aaaaaa

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -6,6 +6,7 @@ public class HelloWorld {
 		System.out.println("Hello World");
 		System.out.println("Hello World hotfix version");
 		System.out.println("Hello World hotfix XXXXXXX");
+		System.out.println("Hello World master AAAAAAA");
 		System.out.println("Hello World hotfix YYYYYYY");
 
 	}
